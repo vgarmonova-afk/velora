d8532f0ca145bcb0cf3dad204a8a097e360e1a2d
vgarmonova-afk <vgarmonova@gmail.com>
2026-09-11 16:00:56 +0300

    added welcome message

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const age = 19;
-const hasAppointment = true;
-const service = "Classic Manicure";
-let discount = 50;
-const price = 500;
-if (age >=18 && hasAppointment===true){
-    console.log(`Ви можете записатися на процедуру ${service}, вартість якої становить ${price} грн.`); 
-}
-if (service === "Gel Polish" || service === "Nail Art"){
-    console.log(`Доступна знижка на послугу, в розмірі ${discount} грн.`); 
-}
-if (hasAppointment === false) {
-    console.log("Ви маєте спочатку записатися.");
-} 
-if (price > 300){
-    console.log("Процедура дорожча 300 грн.");
-} else {
-    console.log("Процедура дешевша 300 грн.");
-}
+const nameInput = document.getElementById("name");
+const form = document.getElementById("form");
+const message = document.getElementById("message");
+const service = document.getElementById("service");
+const date = document.getElementById("date");
+const consent = document.getElementById("consent");
+const price = document.getElementById("price");
+
+service.addEventListener("change", function(){
+
+    if (service.value === "Gel Polish") {
+        price.textContent = "Ціна - 600 грн ";
+    } else if (service.value === "Classic Manicure") {
+        price.textContent = "Ціна - 350 грн";
+      } else if (service.value === "Nail Art") {
+        price.textContent = "Ціна - 800 грн";
+      }
+      price.classList.add("show");
+      setTimeout(function(){
+        price.classList.remove("show");
+      }, 3000 );
+})
+    form.addEventListener("submit", function(event) {
+    event.preventDefault();
+ if (consent.checked) {
+
+    message.textContent = `Дякуємо за вашу заявку, ${nameInput.value}! Ми зв'яжемося з вами найближчим часом. Ви обрали позицію ${service.value} на ${date.value}`;
+    message.classList.add("show");
+    
+    setTimeout(function(){
+        
+        message.classList.remove("show");
+        form.reset();
+    }, 3000 );}
+    else {
+    message.textContent = "Будь ласка погодьтеся на обробку даних перед відправкою форми.";
+    message.classList.add("show");
+    setTimeout(function(){
+        message.classList.remove("show");
+    } , 3000 );
+} }); 
+    
+
+
+    
